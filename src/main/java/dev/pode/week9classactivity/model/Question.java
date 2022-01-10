97c98a33ce26601f0afed070df06ac9547c729a8
Odochi-Okara <85240161+Odochi-Okara@users.noreply.github.com>
2022-01-10 10:11:20 +0100

    first commit

--- src/main/java/dev/pode/week9classactivity/model/Question.java
+++ src/main/java/dev/pode/week9classactivity/model/Question.java
@@ -1,4 +1,14 @@
 package dev.pode.week9classactivity.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Question {
+    private Long id;
+    private String title;
+    private String description;
 }
