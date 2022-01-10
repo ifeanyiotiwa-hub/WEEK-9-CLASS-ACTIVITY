eba33262d04716bd5fec6bc479665e027ce126cb
NJBodam <bodambest@yahoo.com>
2022-01-10 10:22:13 +0100

    question repository
    two methods added to question repository

--- src/main/java/dev/pode/week9classactivity/repository/QuestionRepository.java
+++ src/main/java/dev/pode/week9classactivity/repository/QuestionRepository.java
@@ -1,4 +1,29 @@
 package dev.pode.week9classactivity.repository;
 
+import dev.pode.week9classactivity.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class QuestionRepository {
+
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public QuestionRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public int saveQuestion(Question question) {
+        String sql = "INSERT INTO question (title, description) " +
+                "VALUES (?, ?)";
+        return jdbcTemplate.update( sql, question.getTitle(), question.getDescription());
+    }
+
+    public Question getQuestionById(int id){
+        String sql = "SELECT * FROM person WHERE id=?";
+        return jdbcTemplate.queryForObject(sql, BeanPropertyRowMapper.newInstance(Question.class),id);
+    }
 }
+
+
