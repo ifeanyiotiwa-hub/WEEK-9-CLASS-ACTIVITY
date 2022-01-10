b9725c98286d95f66e6c4dc59b9853fbe2dc3a9d
Odochi-Okara <85240161+Odochi-Okara@users.noreply.github.com>
2022-01-10 10:25:19 +0100

    modified schema file

---
+++ src/main/resources/schema-postgresql.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS question;
+
+CREATE TABLE IF NOT EXISTS question(
+    id serial PRIMARY KEY,
+    title varchar(130) NOT NULL,
+    description varchar(130) NOT NULL,
+
+    );
