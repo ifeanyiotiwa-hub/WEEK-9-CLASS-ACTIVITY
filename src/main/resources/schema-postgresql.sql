e38e3975e6750b37b314d5fd6c0e6cb190589dbe
Odochi-Okara <85240161+Odochi-Okara@users.noreply.github.com>
2022-01-10 10:31:22 +0100

    corrected an error in schema

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS question;
 CREATE TABLE IF NOT EXISTS question(
     id serial PRIMARY KEY,
     title varchar(130) NOT NULL,
-    description varchar(130) NOT NULL,
+    description varchar(130) NOT NULL
 
     );
